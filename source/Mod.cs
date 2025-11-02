606deb6c75a1609d75938610cfac20abe6b783ce
Ahmed Hanie <flubzd@gmail.com>
2025-11-02 22:54:54 +0200

    feat: disable vanilla alert

---
+++ source/Mod.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Verse;
+
+
+namespace SK_Unassigned_Beds_Message
+{
+    public class Mod: Verse.Mod
+    {
+        public static Harmony instance;
+
+        public Mod(ModContentPack content) : base(content)
+        {
+            instance = new Harmony("rimworld.sk.unassignedbedsalert");
+            LongEventHandler.QueueLongEvent(Init, "Sk.Unassigned_Beds_Message.Init", doAsynchronously: true, null);
+        }
+
+        public static void Init()
+        {
+            instance.PatchAll();
+        }
+    }
+}
---
+++ source/Patches/Alert_NeedColonistBeds.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace SK_Unassigned_Beds_Message.Patches
+{
+    public static class Alert_NeedColonistBedsPatches
+    {
+        [HarmonyPatch(typeof(Alert_NeedColonistBeds), "GetReport")]
+        public static class GetReport
+        {
+            public static bool Prefix()
+            {
+                return false;
+            }
+        }
+    }
+}
